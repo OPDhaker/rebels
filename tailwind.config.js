e735aad35a34973f8d432b46ee8e1ee15ad0274e
OPDhaker <OPDhaker2007@gmail.com>
2022-10-21 22:57:39 +0530

    INITIAL

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: "'Roboto Mono', monospace",
+      }
+    },
+  },
+  plugins: [],
+}
